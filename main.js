70b678372ab21285d9815eea444a746cae34ecb3
nikitsch <nikitsch5@gmail.com>
2023-06-15 22:04:27 +0300

    feat: openai

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 import express from "express";
+import config from "config"
 import { engine } from "express-handlebars";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: config.get("OPENAI_API_KEY"),
+});
+const openai = new OpenAIApi(configuration);
 
 const app = express();
 
@@ -9,7 +16,33 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_, res) => {
-  res.render("index")
+  res.render("index");
 })
+
+app.post("/", async (req, res) => {
+  const prompt = req.body.prompt
+  const size = req.body.size ?? "512x512"
+  const number = req.body.number ?? 1
+
+  try {
+    const response = await openai.createImage({
+      prompt,
+      size,
+      n: Number(number)
+    });
+    // console.log(response.data.data);
+
+    res.render("index", {
+      images: response.data.data
+    })
+
+  } catch (error) {
+    console.log(error.response.data);
+    res.render("index", {
+      error: error.message
+    });
+  }
+
+});
 
 app.listen(3000, () => console.log("Server started..."));
